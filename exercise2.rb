f770b5447ca395f438115237d629d161ab5f9412
Jay Deverett <jayjdeverett@gmail.com>
2017-06-26 13:55:33 -0400

    add assignment part 2

---
+++ exercise2.rb
@@ -0,0 +1,29 @@
+# Problem 1
+#
+puts (55 * 1.18).round
+#
+# Answer = 65
+
+
+# Problem 2
+#
+#puts "ABC" + 123 (=Typeerror)
+puts "ABC" + 123.to_s
+#
+# Answer = ABC123
+
+
+# Problem 3
+#
+x = 45628 * 7839
+#
+puts "The answer to Problem 3 is #{x}"
+#
+# Answer = "The answer to Problem 3 is 357677892"
+
+
+# Problem 4
+#
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+#
+# Answer = true
