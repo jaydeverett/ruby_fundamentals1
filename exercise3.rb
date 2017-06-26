07ba1a942fed1a3717269eca0ce2984a96e52e7d
Jay Deverett <jayjdeverett@gmail.com>
2017-06-26 14:14:42 -0400

    add age calculation to exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -2,4 +2,11 @@ puts "What is your name?"
 
 user_name = gets.chomp
 
-puts "Hello, #{user_name}"
+
+puts "What year were you born?"
+
+birth_year = (gets.chomp).to_i
+
+age = 2017 - birth_year
+
+puts "Hi #{user_name}. You are #{age} years old and were born in #{birth_year}!"
